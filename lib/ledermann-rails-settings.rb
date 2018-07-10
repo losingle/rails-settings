37c88e0705e8b622b0bc04fb09d9a411655237e0
losingle <losingle@gmail.com>
2018-07-10 15:37:47 +0800

    FIX module name

--- lib/ledermann-rails-settings.rb
+++ lib/ledermann-rails-settings.rb
@@ -1,4 +1,4 @@
-module RailsSettings
+module LedermannRailsSettings
   # In Rails 3, attributes can be protected by `attr_accessible` and `attr_protected`
   # In Rails 4, attributes can be protected by using the gem `protected_attributes`
   # In Rails 5, protecting attributes is obsolete (there are `StrongParameters` only)
